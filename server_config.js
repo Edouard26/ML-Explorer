9160e08031bb368ac6ede90cbb857af07e3047dd
Edouard26 <8801@holbertonstudents.com>
2024-11-28 18:05:11 +0100

    starting configure Node.js && Server file / still some  issues to fix

---
+++ server_config.js
@@ -0,0 +1,17 @@
+     // server.js
+     const express = require('express');
+     const path = require('path');
+     const app = express();
+
+     // Servir les fichiers statiques de l'application React
+     app.use(express.static(path.join(__dirname, 'build')));
+
+     // Pour toute demande, servir l'index.html
+     app.get('*', (req, res) => {
+       res.sendFile(path.join(__dirname, 'build', 'index.html'));
+     });
+
+     const PORT = process.env.PORT || 3000;
+     app.listen(PORT, () => {
+       console.log(`Server is running on port ${PORT}`);
+     });
